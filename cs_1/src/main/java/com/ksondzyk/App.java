28a25b724b13938105ad9369b8d65ec7a7de441c
Max Ksondzyk <maxksiondzyk@gmail.com>
2020-05-16 09:46:10 +0300

    some stuff

--- cs_1/src/main/java/com/ksondzyk/App.java
+++ cs_1/src/main/java/com/ksondzyk/App.java
@@ -1,6 +1,10 @@
 package com.ksondzyk;
 
+import lombok.var;
+
 import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
 
 /**
  * Hello world!
@@ -8,13 +12,21 @@ import java.io.IOException;
  */
 public class App 
 {
-    public static void main(String[] args) {
-        try {
+    public App() throws IOException {
+    }
+
+    public static void main(String[] args) throws IOException {
+            if (args.length != 1) {
+                System.err.println("Pass the server IP as the sole command line argument");
+                return;
+            }
+            var socket = new Socket(args[0], 59090);
+            var in = new Scanner(socket.getInputStream());
+            System.out.println("Server response: " + in.nextLine());
+
             Packet packet = new Packet((byte) 1,100,2456,"hello");
             PacketReceiver packetReceiver = new PacketReceiver(packet.getData());
             System.out.println("Message: "+packetReceiver.getMessage()+"\nPacket is intact: "+packetReceiver.checkCRC());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
     }
 }
