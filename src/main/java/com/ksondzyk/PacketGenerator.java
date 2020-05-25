dda4f48ebf9a60ff8884730710394228ab0d4eb4
Max Ksondzyk <maxksiondzyk@gmail.com>
2020-05-25 16:09:31 +0300

    added clientThread

--- src/main/java/com/ksondzyk/PacketGenerator.java
+++ src/main/java/com/ksondzyk/PacketGenerator.java
@@ -7,7 +7,7 @@ import java.util.Random;
 
 public class PacketGenerator {
 
-    Packet newPacket(){
+    public Packet newPacket(){
         Random rand;
         byte[] array;
         array = new byte[7];
--- src/main/java/com/ksondzyk/network/TCPNetwork/ClientThread.java
+++ src/main/java/com/ksondzyk/network/TCPNetwork/ClientThread.java
@@ -1,5 +1,6 @@
 package com.ksondzyk.network.TCPNetwork;
 
+import com.ksondzyk.PacketGenerator;
 import com.ksondzyk.PacketReceiver;
 import com.ksondzyk.Processor;
 import com.ksondzyk.entities.Packet;
@@ -8,44 +9,54 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Arrays;
 
-public class ServerThread extends Thread {
+public class ClientThread extends Thread {
 
     private InputStream is;
     private OutputStream os;
     private Socket socket;
 
-    ServerThread(Socket socket){
+    ClientThread(Socket socket){
+        socket = serverSocket.accept();
         this.socket= socket;
 
         try {
             is = socket.getInputStream();
             os = socket.getOutputStream();
 
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-      //  System.out.println("Thread is running");
 
-        //this.start();
     }
 
     public void run(){
+
         //while (true) {
 
             try {
-                System.out.println("Thread is running");
+                PacketGenerator pg = new PacketGenerator();
+                Packet packet = pg.newPacket();
+                byte[] packetBytes = packet.getData();
+
+                os.write(packetBytes);
+                os.flush();
+
+                System.out.println("Send"+currentThread().getName());
+
                 PacketReceiver pr = new PacketReceiver();
-                Packet packet = null;
-                try {
-                    packet = pr.receive(is);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                Packet packetReceived = pr.receive(is);
 
                 System.out.println("Received"+currentThread().getName());
+                System.out.println(Arrays.toString(packetBytes) + "\n");
 
-                System.err.println(packet.getMessage());
+                System.err.println(packetReceived.getMessage());
+                //Processor.process(this, packet);
+
+
+
 
                 Processor.process(packet, os);
             }catch (Exception e){
