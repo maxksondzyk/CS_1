f6de037b8d45fe50161c5922d3ce37ff0a495cb8
jackkaroo <mmmekaterina2013@gmail.com>
2020-06-24 17:36:27 +0300

    test get and put

--- src/main/resources/templates/assets/javascript/simple.js
+++ src/main/resources/templates/assets/javascript/simple.js
@@ -1,28 +1,49 @@
 console.log("success");
+let tokenCookie = getCookie('token');
+console.log(tokenCookie)
+
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+/*PUT*/
 
 
-// fetch("/login?login=me&password=1a1dc91c907325c69271ddf0c944bc72", {
-// method: "GET",
-// headers: {
-// 'Content-Type': 'application/json'
-// }
-// }
-// ).then(function(res) {
-//         console.log(res.json())
+let obj = {
+    "title": "tomatoes1",
+    "category": "vegetables1",
+    "quantity": "1021",
+    "price": "951"
+}
+
+// fetch("/api/good", {
+//         method: "PUT",
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'token': tokenCookie
+//         },
+//         body: JSON.stringify(obj)
 //     }
-//
-// )
-
-// fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/JFK-sky/2019-09-01?inboundpartialdate=2019-12-01", {
-//     "method": "GET",
-//     "headers": {
-//         "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-//         "x-rapidapi-key": "45266a2716msh77748bd81cec01dp1d3795jsn903133b56017"
-//     }
+// ).then(function(response) {
+//     response.json().then(function (data) {
+//         console.log(data)
+//     })
 // })
-//     .then(function(response){
-//         response.json().then(function (data) {
-//             console.log(data)
-//         })
-//
-//     });
+
+/*GET*/
+
+// fetch("/api/good/1", {
+//         method: "GET",
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'token': tokenCookie
+//         }
+//     }
+// ).then(function(response) {
+//     response.json().then(function (data) {
+//         console.log(data)
+//     })
+// })
+
